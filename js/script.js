000deba175600e016c94f3e9027a071ba6f02d55
Verta-master <vertyagina@gmail.com>
2018-09-26 16:22:56 +0600

    Main

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//Mobile menu
+$('.menu-btn').click(function() {
+  $('#nav_bottom').toggleClass('menu--opened');
+  $('#nav_bottom ul').slideToggle();
+});
+
+$('.nav-btn').click(function() {
+  $('#nav_top').toggleClass('nav_top--opened');
+//  $('#nav-top ul').toggleClass('nav_list');
+});
